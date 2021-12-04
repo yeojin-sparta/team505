dfca095a3be1669cfe80e3c89648f5b735e7a1ad
Yeojin Kim <yeojin.dev@gmail.com>
2021-12-04 10:30:05 +0900

    커밋 테스트

--- week04/music_scrap.py
+++ week04/music_scrap.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-
+# test
 def print_song(date, rate):
     """특정 날짜의 특정 순위 곡을 출력한다
 
@@ -12,7 +12,7 @@ def print_song(date, rate):
     """
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    # in python(f-string)
+    # in python(f-string)!
     url = f'https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=202111{date}'
 
     data = requests.get(url, headers=headers)
